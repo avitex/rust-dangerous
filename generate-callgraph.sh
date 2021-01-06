4c1673da6cf734ac7facdf9e7350ab9e92d482c9
avitex <theavitex@gmail.com>
2021-01-06 16:17:03 +1100

    move benches to separate crate

--- generate-callgraph.sh
+++
@@ -1,5 +0,0 @@
-cargo rustc --example safe --release -- --emit=llvm-ir
-cat target/release/examples/safe*.ll | opt -analyze -dot-callgraph
-dot -Tsvg -ocallgraph.svg '<stdin>.callgraph.dot'
-rm '<stdin>.callgraph.dot'
-rm target/release/examples/safe*.ll
