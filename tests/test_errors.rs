66ec380c3520f5d142f56c34285f2f5f891ae2b6
avitex <theavitex@gmail.com>
2021-01-10 20:13:51 +1100

    test error input str and invalid span

--- tests/test_errors.rs
+++ tests/test_errors.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 #[macro_use]
 mod common;
 
@@ -115,25 +117,25 @@ fn test_expected_valid_root() {
 
 #[test]
 #[cfg(feature = "full-context")]
-fn test_expected_valid_full_debug() {
-    let error = input!(b"123\xC2 ")
+fn test_expected_valid_full_debug_str() {
+    let error = input!(b"123\n\xC2 ")
         .read_all::<_, _, Expected>(|r| r.context("hi", |r| r.take_str_while(|_| true)))
         .unwrap_err();
 
     assert_eq!(
-        format!("{:?}", error),
+        format!("{:#?}", error),
         concat!(
             "\n-- INPUT ERROR ---------------------------------------------\n",
             "error attempting to take str while: expected utf-8 code point\n",
-            "> [31 32 33 c2 20]\n",
-            "            ^^    \n",
+            "> ['1' '2' '3' '\\n' c2 20]\n",
+            "                    ^^    \n",
             "additional:\n",
-            "  error offset: 3, input length: 5\n",
+            "  error line: 2, error offset: 4, input length: 6\n",
             "backtrace:\n",
             " 1. `read all`\n",
             " 2. `read` (expected hi)\n",
-            " 3. `take str while` (expected utf-8 code point)",
-            "\n------------------------------------------------------------\n",
+            " 3. `take str while` (expected utf-8 code point)\n",
+            "------------------------------------------------------------\n"
         )
     );
 }
@@ -226,6 +228,59 @@ fn test_expected_value_root() {
             "  error offset: 0, input length: 3\n",
             "backtrace:\n",
             " 1. `consume` (expected exact value)"
+        )
+    );
+}
+
+#[test]
+fn test_invalid_error_details_span() {
+    use dangerous::display::{ErrorDisplay, Write};
+    use dangerous::error::{
+        ContextStack, ContextStackBuilder, Details, ExpectedValid, RootContextStack,
+    };
+    use dangerous::Input;
+
+    struct MyError(RootContextStack);
+
+    impl<'i> From<ExpectedValid<'i>> for MyError {
+        fn from(err: ExpectedValid<'i>) -> Self {
+            Self(RootContextStack::from_root(err.context()))
+        }
+    }
+
+    impl<'i> Details<'i> for MyError {
+        fn input(&self) -> Input<'i> {
+            input!(b"something")
+        }
+        fn span(&self) -> Input<'i> {
+            input!(b"not-a-proper-span")
+        }
+        fn expected(&self) -> Option<Input<'_>> {
+            None
+        }
+        fn description(&self, w: &mut dyn Write) -> fmt::Result {
+            w.write_str("test")
+        }
+        fn context_stack(&self) -> &dyn ContextStack {
+            &self.0
+        }
+    }
+
+    let error = input!(b"\xC2 ").to_dangerous_str::<MyError>().unwrap_err();
+
+    assert_eq!(
+        format!("{}", ErrorDisplay::new(&error))
+            .splitn(2, "additional:")
+            .next()
+            .unwrap(),
+        concat!(
+            "error attempting to convert input to str: test\n",
+            "note: error span is not within the error input indicating the\n",
+            "      concrete error being used has a bug. Consider raising an\n",
+            "      issue with the maintainer!\n",
+            "span:\n",
+            "> [6e 6f 74 2d 61 2d 70 72 6f 70 65 72 2d 73 70 61 6e]\n",
+            "input:\n> [73 6f 6d 65 74 68 69 6e 67]\n",
         )
     );
 }
