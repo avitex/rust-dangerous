44674590ebaf19495a09111e5ad3a86f98f2e12c
avitex <theavitex@gmail.com>
2021-01-10 15:45:10 +1100

    fix fuzzing

--- fuzz/fuzz_targets/general.rs
+++ fuzz/fuzz_targets/general.rs
@@ -22,7 +22,7 @@ fuzz_target!(|data: &[u8]| {
     let (input_a, input_b) = if input_full.is_empty() {
         (input_full.clone(), input_full.clone())
     } else {
-        let (a, b) = data.split_at(rng.gen_range(0, data.len()));
+        let (a, b) = data.split_at(rng.gen_range(0..data.len()));
         (dangerous::input(a), dangerous::input(b))
     };
 
@@ -46,8 +46,11 @@ fuzz_target!(|data: &[u8]| {
     read_partial!(input_full.clone(), |r| r.skip(rng.gen()));
     read_partial!(input_full.clone(), |r| Ok(r.take_while(|c| c == rng.gen())));
     read_partial!(input_full.clone(), |r| Ok(r.skip_while(|c| c == rng.gen())));
+    read_partial!(input_full.clone(), |r| r.take_str_while(|c| c == rng.gen()));
+    read_partial!(input_full.clone(), |r| r.skip_str_while(|c| c == rng.gen()));
     read_partial!(input_full.clone(), |r| r.try_take_while(|c| Ok(c == rng.gen())));
-    read_partial!(input_full.clone(), |r| r.try_skip_while(|c| Ok(c == rng.gen())));
+    read_partial!(input_full.clone(), |r| r.try_take_str_while(|c| Ok(c == rng.gen())));
+    read_partial!(input_full.clone(), |r| r.try_skip_str_while(|c| Ok(c == rng.gen())));
     read_partial!(input_full, |r| r.consume(single_slice));
 });
 
@@ -63,8 +66,8 @@ impl fmt::Write for DummyWrite {
 }
 
 fn derive_rng(seed: &[u8]) -> SmallRng {
-    let mut arr = [0u8; 16];
-    for (i, b) in seed.iter().copied().take(16).enumerate() {
+    let mut arr = [0u8; 32];
+    for (i, b) in seed.iter().copied().take(32).enumerate() {
         arr[i] = b;
     }
     SmallRng::from_seed(arr)
