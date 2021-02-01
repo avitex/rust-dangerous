459ec5bd7951c309cc5491f3343266d6c379738f
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-02-01 17:28:40 +0800

    Fix examples to pass `#![deny(rust_2018_idionms)]`

--- examples/debug.rs
+++ examples/debug.rs
@@ -3,14 +3,14 @@ use dangerous::{Expected, Input};
 fn main() {
     // Expect length
     dbg!(&dangerous::input(b"A\xC3\xA9 \xC2")
-        .to_dangerous_str::<Expected>()
+        .to_dangerous_str::<Expected<'_>>()
         .unwrap_err());
     // Expect valid
     dbg!(&dangerous::input(b"A\xC3\xA9 \xFF")
-        .to_dangerous_str::<Expected>()
+        .to_dangerous_str::<Expected<'_>>()
         .unwrap_err());
     // Expect value
     dbg!(&dangerous::input(b"hello")
-        .read_all::<_, _, Expected>(|r| r.consume(b"world"))
+        .read_all::<_, _, Expected<'_>>(|r| r.consume(b"world"))
         .unwrap_err());
 }
