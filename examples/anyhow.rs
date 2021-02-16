94d90935bd9710d90138f648f7f8fdcdeb2fafd1
avitex <theavitex@gmail.com>
2021-02-16 12:05:55 +1100

    add anyhow example

---
+++ examples/anyhow.rs
@@ -0,0 +1,18 @@
+use dangerous::{BytesReader, Error, Expected, Input};
+use anyhow::Context;
+
+fn main() {
+    dangerous::input(b"hello")
+        .read_all::<_, _, Expected<'_>>(parse)
+        .map_err(|error| anyhow::Error::msg(error.to_string()))
+        .context("my anyhow context 1")
+        .context("my anyhow context 2")
+        .unwrap();
+}
+
+fn parse<'i, E>(r: &mut BytesReader<'i, E>) -> Result<(), E>
+where
+    E: Error<'i>,
+{
+    r.context("my value", |r| r.consume(b"world"))
+}
