6b313eeaa4a3ab8674040f188de783abeb20b786
avitex <theavitex@gmail.com>
2021-07-12 14:57:23 +1000

    fix fuzz

--- fuzz/fuzz_targets/general.rs
+++ fuzz/fuzz_targets/general.rs
@@ -1,10 +1,7 @@
 #![no_main]
+
+use dangerous::{Expected, Input};
 use libfuzzer_sys::fuzz_target;
-
-use core::fmt::{self, Write as _};
-
-use dangerous::{Input, Expected};
-use rand::{rngs::SmallRng, Rng, SeedableRng};
 
 macro_rules! read_partial {
     ($input:expr, $read_fn:expr) => {
@@ -12,80 +9,38 @@ macro_rules! read_partial {
     };
 }
 
-fuzz_target!(|data: &[u8]| {
-    let mut rng = derive_rng(data);
+fuzz_target!(|data_length_and_patterns: (&[u8], usize, u8, &[u8])| {
+    let (data, len, byte_pattern, slice_pattern) = data_length_and_patterns;
+    let input = dangerous::input(data);
 
-    let single_byte: u8 = rng.gen();
-    let single_slice = &[rng.gen()][..];
-
-    let input_full = dangerous::input(data);
-
-    let (input_a, input_b) = if input_full.is_empty() {
-        (input_full.clone(), input_full.clone())
-    } else {
-        let (a, b) = data.split_at(rng.gen_range(0..data.len()));
-        (dangerous::input(a), dangerous::input(b))
-    };
-
-    let _ = input_a.is_within(&input_b);
-
-    if let Err(err) = input_full.to_dangerous_str::<Expected>() {
-        write!(DummyWrite, "{}", err).unwrap();
-    }
-
-    write!(DummyWrite, "{}", input_full.display().full()).unwrap();
-    write!(DummyWrite, "{}", input_full.display().head(rng.gen())).unwrap();
-    write!(DummyWrite, "{}", input_full.display().tail(rng.gen())).unwrap();
-    write!(DummyWrite, "{}", input_full.display().head_tail(rng.gen())).unwrap();
-    write!(DummyWrite, "{}", input_full.display().span(&input_a, rng.gen())).unwrap();
     // read
-    read_partial!(input_full.clone(), |r| r.read_u8());
+    read_partial!(input.clone(), |r| r.read_u8());
     // peek
-    read_partial!(input_full.clone(), |r| r.peek_u8());
-    read_partial!(input_full.clone(), |r| r.peek(1).map(drop));
-    read_partial!(input_full.clone(), |r| Ok(r.peek_eq(single_slice)));
+    read_partial!(input.clone(), |r| r.peek_u8());
+    read_partial!(input.clone(), |r| r.peek(len).map(drop));
+    read_partial!(input.clone(), |r| Ok(r.peek_eq(slice_pattern)));
     // consume
-    read_partial!(input_full.clone(), |r| r.consume(single_byte));
-    read_partial!(input_full.clone(), |r| r.consume(single_slice));
+    read_partial!(input.clone(), |r| r.consume(byte_pattern));
+    read_partial!(input.clone(), |r| r.consume(slice_pattern));
     // take/skip
-    read_partial!(input_full.clone(), |r| r.take(rng.gen()));
-    read_partial!(input_full.clone(), |r| r.skip(rng.gen()));
+    read_partial!(input.clone(), |r| r.take(len));
+    read_partial!(input.clone(), |r| r.skip(len));
     // (take/skip/try_take/try_skip)_while
-    read_partial!(input_full.clone(), |r| Ok(r.skip_while(rng.gen::<u8>())));
-    read_partial!(input_full.clone(), |r| Ok(r.skip_while(input_a.as_dangerous())));
-    read_partial!(input_full.clone(), |r| Ok(r.take_while(|c| c == rng.gen())));
-    read_partial!(input_full.clone(), |r| Ok(r.skip_while(|c| c == rng.gen())));
-    read_partial!(input_full.clone(), |r| r.try_take_while(|c| Ok(c == rng.gen())));
-    read_partial!(input_full.clone(), |r| r.try_skip_while(|c| Ok(c == rng.gen())));
+    read_partial!(input.clone(), |r| Ok(r.skip_while(byte_pattern)));
+    read_partial!(input.clone(), |r| Ok(r.skip_while(slice_pattern)));
+    read_partial!(input.clone(), |r| Ok(r.take_while(|c| c == byte_pattern)));
+    read_partial!(input.clone(), |r| Ok(r.skip_while(|c| c == byte_pattern)));
+    read_partial!(input.clone(), |r| r
+        .try_take_while(|c| Ok(c == byte_pattern)));
+    read_partial!(input.clone(), |r| r
+        .try_skip_while(|c| Ok(c == byte_pattern)));
     // (take/skip)_until
-    read_partial!(input_full.clone(), |r| Ok(r.take_until(rng.gen::<u8>())));
-    read_partial!(input_full.clone(), |r| Ok(r.take_until(input_a.as_dangerous())));
-    read_partial!(input_full.clone(), |r| Ok(r.take_until_opt(|c| c == rng.gen())));
-    read_partial!(input_full.clone(), |r| Ok(r.take_until_opt(|c| c == rng.gen())));
-    // (take/skip/try_take/try_skip)_str_while
-    read_partial!(input_full.clone(), |r| r.take_str_while(|c| c == rng.gen()));
-    read_partial!(input_full.clone(), |r| r.skip_str_while(|c| c == rng.gen()));
-    read_partial!(input_full.clone(), |r| r.try_take_str_while(|c| Ok(c == rng.gen())));
-    read_partial!(input_full.clone(), |r| r.try_skip_str_while(|c| Ok(c == rng.gen())));
-
-    let _ = input_full;
+    read_partial!(input.clone(), |r| Ok(r.take_until(byte_pattern)));
+    read_partial!(input.clone(), |r| Ok(r.take_until(slice_pattern)));
+    read_partial!(input.clone(), |r| Ok(
+        r.take_until_opt(|c| c == byte_pattern)
+    ));
+    read_partial!(input.clone(), |r| Ok(
+        r.take_until_opt(|c| c == byte_pattern)
+    ));
 });
-
-///////////////////////////////////////////////////////////////////////////////
-// Support
-
-struct DummyWrite;
-
-impl fmt::Write for DummyWrite {
-    fn write_str(&mut self, _: &str) -> fmt::Result {
-        Ok(())
-    }
-}
-
-fn derive_rng(seed: &[u8]) -> SmallRng {
-    let mut arr = [0u8; 32];
-    for (i, b) in seed.iter().copied().take(32).enumerate() {
-        arr[i] = b;
-    }
-    SmallRng::from_seed(arr)
-}
